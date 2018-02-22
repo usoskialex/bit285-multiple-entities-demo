5f39fa88edba977d91b87e12cda2c0b6c732fcd9
Brian Bansenauer <brianb@cascadia.edu>
2018-02-22 10:44:57 -0800

    Add SalesVM

---
+++ bit285-multiple-entities-demo/ViewModels/SalesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndyBooks.ViewModels
+{
+    public class SalesViewModel
+    {
+        //TODO: Add properties to support a View listing Titles and Amounts 
+
+    }
+}
